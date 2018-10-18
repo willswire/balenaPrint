f86111d9b46fc6d3e8c9a95080c82f57cfd1f2e1
Will Walker <wwalker@udel.edu>
2018-10-18 13:53:01 -0400

    cleaning up

--- setup.sh
+++ setup.sh
@@ -2,18 +2,28 @@
 
 # Copy default cups config files if /data/cups is empty
 if [ "$(ls -A /data/cups 2> /dev/null)" == "" ]; then
+    echo "Looks like /data/ doesn't have CUPS.."
     mkdir /data/cups
     mv /usr/src/cups/* /data/cups
+    echo "Moved default config files to persistent /data/"
 fi
 
 # Clear any contents made by the default CUPS installation
+echo "Clearing default CUPS configuration files"
 rm -r /etc/cups
 
 # Establish SymLink for persistent /data/
+echo "Establishing SymLink for CUPS..."
 ln -s /data/cups /etc/cups
+echo "Establishing SymLink for server.crt"
+ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem /etc/cups/ssl/server.crt
+echo "Establishing SymLink for server.key"
+ln -s /etc/ssl/private/ssl-cert-snakeoil.key /etc/cups/ssl/server.key
 
 # Restart CUPS
+echo "Initialization complete: Restarting CUPS"
 /etc/init.d/cups restart
 
 # Change password to defaultpassword for root user
+echo "Setting default password for CUPS"
 echo -e "defaultpassword\ndefaultpassword" | (passwd root)
