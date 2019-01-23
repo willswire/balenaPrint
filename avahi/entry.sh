ec284aa6959bfe1f8c72360d72c40fdb28fd8b14
Will Walker <willwalker@wifi-roaming-128-4-78-184.host.udel.edu>
2019-01-23 16:20:55 -0500

    restate entry.sh

--- avahi/entry.sh
+++ avahi/entry.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # Start dbus
-echo "Starting dbus service..."
-/etc/init.d/dbus start
-
-# Start dbus
-echo "Starting avahi service..."
-/etc/init.d/avahi-daemon start
+echo "Starting dbus and avahi services..."
+dbus-daemon & avahi-daemon --no-rlimits --no-drop-root
