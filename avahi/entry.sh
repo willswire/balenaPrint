93614b0dc59e9d68d60f82ac5ccdc5fd0d2441f5
willswire <wwalker@udel.edu>
2019-02-18 14:01:07 -0500

    rm PID

--- avahi/entry.sh
+++ avahi/entry.sh
@@ -2,5 +2,5 @@
 
 # Start avahi
 echo "Starting avahi service..."
-rm -rf /etc/avahi/services/ssh.service
+rm -rf /etc/avahi/services/ssh.service /var/run/avahi-daemon/pid
 avahi-daemon --no-rlimits --no-drop-root
