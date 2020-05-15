a144e4e76b3b7feafe02665d1ffe07ea81b02c20
headlesscow <pirate@headlesscow.com>
2020-05-15 12:14:08 -0400

    Add autoconfiguration of HP network printers

--- cups/run.sh
+++ cups/run.sh
@@ -9,4 +9,32 @@ iptables -A INPUT -p tcp --dport 80 -i resin-vpn -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j REJECT
 
 echo "Starting CUPS"
-cupsd -f 
+cupsd -f &
+
+
+echo "Configuring HP Printers"
+if [[ -n "$HP_IP_PRINTERS" ]]; then
+  printers=$(echo $HP_IP_PRINTERS | tr ";" "\n")
+  for printer in $printers
+  do
+    echo "Make URI for $printer"
+    url=$(hp-makeuri -c $printer)
+    if [ $? -eq 0 ]; then
+      echo "Found URL: $url"
+      echo "Checking to see if printer is in CUPS already"
+      lpstat -v | grep -Fe "$url"
+      if [ $? -ne 0 ]; then
+        echo "Configuring [$printer]"
+        hp-setup -i -a -x $printer
+      else
+        echo "Printer $printer already in CUPS with url: $url"
+      fi
+    else
+      echo "Couldn't find printer on network: $printer"
+    fi 
+  done
+else
+  echo "No HP IP Printers to configure"
+fi
+
+wait
